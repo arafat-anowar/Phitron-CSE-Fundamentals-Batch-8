a232714b5758dfd41e2d879adf35a30484a705de
MD ARAFAT ANOWAR <arafat-anowar@outlook.com>
2026-07-14 22:04:56 +0600

    Assignment - 1

---
+++ Semester 1/Introduction to Programming Language/Week 1/Module 4 (Assignment 1)/Divisible.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int num;
+    scanf("%d",&num);
+
+    if (num%3==0)
+    {
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+    return 0;
+}
---
+++ Semester 1/Introduction to Programming Language/Week 1/Module 4 (Assignment 1)/Multiply.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+
+    long long int mul=(long long int)a*b;
+
+    printf("%lld",mul);
+    return 0;
+}
